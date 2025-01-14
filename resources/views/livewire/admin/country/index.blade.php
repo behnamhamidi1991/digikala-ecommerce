3e8fcc2a5283371eaf8abe100923e07653078f0a
= <eslp91@hotmail.com>
2025-01-14 20:53:33 +0330

    dashboard

---
+++ resources/views/livewire/admin/country/index.blade.php
@@ -0,0 +1,70 @@
+<div class="col-lg-12 col-12  layout-spacing">
+    <div class="statbox widget box box-shadow">
+        <div class="widget-header">
+            <div class="row">
+                <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                    <h4>Disabled Fields</h4>
+                </div>
+            </div>
+        </div>
+        <div class="widget-content widget-content-area">
+            <form>
+                <fieldset disabled>
+                    <div class="form-group mb-4">
+                        <label for="disabledTextInput">Disabled input</label>
+                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                    </div>
+                    <div class="form-group mb-4">
+                        <label for="disabledSelect">Disabled select menu</label>
+                        <select id="disabledSelect" class="form-control">
+                            <option>Disabled select</option>
+                        </select>
+                    </div>
+                    <div class="form-group mb-4">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Can't check this
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-0">Submit</button>
+                </fieldset>
+            </form>
+
+            <div class="code-section-container">
+
+                <button class="btn toggle-code-snippet"><span>Code</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down toggle-code-icon"><polyline points="6 9 12 15 18 9"></polyline></svg></button>
+
+                <div class="code-section text-left">
+              <pre>
+            &lt;form&gt;
+                &lt;fieldset disabled&gt;
+                    &lt;div class="form-group mb-4"&gt;
+                        &lt;label for="disabledTextInput"&gt;Disabled input&lt;/label&gt;
+                        &lt;input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input"&gt;
+                    &lt;/div&gt;
+                    &lt;div class="form-group mb-4"&gt;
+                        &lt;label for="disabledSelect"&gt;Disabled select menu&lt;/label&gt;
+                        &lt;select id="disabledSelect" class="form-control"&gt;
+                            &lt;option&gt;Disabled select&lt;/option&gt;
+                        &lt;/select&gt;
+                    &lt;/div&gt;
+                    &lt;div class=&quot;form-group mb-4&quot;&gt;
+                        &lt;div class=&quot;form-check&quot;&gt;
+                            &lt;input class=&quot;form-check-input&quot; type=&quot;checkbox&quot; value=&quot;&quot; id=&quot;flexCheckDefault&quot;&gt;
+                            &lt;label class=&quot;form-check-label&quot; for=&quot;flexCheckDefault&quot;&gt;
+                                Can't check this
+                            &lt;/label&gt;
+                        &lt;/div&gt;
+                    &lt;/div&gt;
+                    &lt;button type="submit" class="btn btn-primary mt-0"&gt;Submit&lt;/button&gt;
+                &lt;/fieldset&gt;
+            &lt;/form&gt;
+</pre>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Livewire\Admin\Dashboard\Index as DashboardIndex;
+use \App\Livewire\Admin\Country\Index as CountryIndex;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin', DashboardIndex::class)->name('admin.dashboard.index');
+Route::get('/admin/country', CountryIndex::class)->name('admin.country.index');
+
